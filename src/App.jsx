4b9453317a2837c91da33ea32ee304f61ad5e265
VictorOduorKe <victoroduorxh2@gmail.com>
2025-07-13 19:31:48 +0300

    added mobile responsive to nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 import About from './components/About'
 import { Projects } from './components/Projects'
 import Skillss from './components/Skillss'
 import Contact from './components/Contact'
 const App = () => {
+  const [menu,setMenu]=useState(false);
   return (
     <>
       <header>
         <h1><a href="/">My Portfolio</a></h1>
         <nav>
-          <ul>
-            <li><a href="#about">About</a></li>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+          <div className="nav-btn" onClick={()=>(setMenu(!menu))}>
+            <i className='fa fa-bars'></i>
+          </div>
+          <ul className={menu?"show-nav":"nav-list"}>
+            <li onClick={()=>(setMenu(!menu))}><a href="#about">About</a></li>
+            <li onClick={()=>(setMenu(!menu))}><a href="#skills">Skills</a></li>
+            <li onClick={()=>(setMenu(!menu))}><a href="#projects">Projects</a></li>
+            <li onClick={()=>(setMenu(!menu))}><a href="#contact">Contact</a></li>
           </ul>
         </nav>
       </header>
@@ -38,7 +42,10 @@ const App = () => {
           <a href="https://www.linkedin.com/in/victor-oduor-5494" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-linkedin"></i>
           </a>
-          <a href="https://www.twitter.com/victor.oduor.5494" target="_blank" rel="noopener noreferrer"></a>
+          <a href="https://www.twitter.com/victor.oduor.5494" target="_blank" rel="noopener noreferrer">
+  <i className="fab fa-twitter"></i>
+</a>
+
         </div>
       </footer>
     </>
